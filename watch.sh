639506ce9688e2914c7459a66db615316842adb0
Will <667@onezerohosting.com>
2023-02-20 17:52:33 -0800

    update watch.sh

--- watch.sh
+++ watch.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-packageDir=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Jenkinsfile
-devDir=~/code/sublime-Jenkinsfile/
+packageDirDarwin=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Jenkinsfile
+packageDirLinux=~/.config/sublime-text/Packages/Jenkinsfile
+
+if [ $(echo $OSTYPE | egrep "darwin") ]; then
+    packageDir=$packageDirDarwin
+else
+    packageDir=$packageDirLinux
+fi
 
 if [ $(which chokidar) ]; then
     chokidar "**/*.py" -c "rm -fR \"$packageDir\" &&
         sleep 3 &&
-        rsync -avu --exclude=".*" \"$devDir\" \"$packageDir\""
+        rsync -avu --exclude=".*" \"$PWD\" \"$packageDir\""
 else
-    while inotifywait -q --event modify --format '%w' ~/sublime-Jenkinsfile/*.py; do
-    cd ~/.config/sublime-text/Packages
-    rm -fR Jenkinsfile
+    while inotifywait -q --event modify --format '%w' ${PWD}/*.py; do
+    rm -fR "$packageDir"
     echo Detected change.
     sleep 3;
-    rsync -avu ~/sublime-Jenkinsfile/ Jenkinsfile
-    ls -la ~/.config/sublime-text/Packages
+    rsync -avu --exclude=".*" "${PWD}/" "$packageDir"
     done
 fi
